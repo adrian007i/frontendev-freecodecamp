a5ed657d1eda78fd6867bbe003aa16f94f93da02
Adrian John <adrianjohn.developer@gmail.com>
2022-08-15 15:44:03 -0400

    drumpad completed

--- src/containers/DrumMachine.js
+++ src/containers/DrumMachine.js
@@ -1,5 +1,54 @@
-const MarkdownPreviewer = ()=>{
+import "../css/DrumMachine.css"
 
-    return <h1>DrumMachine</h1>
+const MarkdownPreviewer = () => {
+
+    const letters = {
+        'Q': "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+        'W': "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+        'E': "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+        'A': "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+        'S': "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+        'D': "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+        'Z': "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+        'X': "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+        'C': "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+    };
+
+    const clickDrum = (key) => {  
+        document.getElementById(key).play()
+        document.getElementById("display").innerHTML = key
+    }
+
+    document.body.addEventListener("keydown", function (event) {
+        clickDrum(event.key.toUpperCase())
+    });
+
+
+    return (
+        <div className="app_block">
+            <div id="drum-machine">
+                <div >
+                    { Object.keys(letters).map((key) => 
+                    <DrumPad letter={key}  key={key} src={letters[key]} clickDrum={clickDrum} /> ) }
+                </div>
+                <div className="instructions" id="display">Click or Press Key</div>
+            </div>
+        </div>
+
+    )
 }
-export default MarkdownPreviewer 
+
+const DrumPad = ({ letter , src, clickDrum }) => { 
+    return (
+        
+        <button className="drum-pad" id={'d_' + letter} onClick={() => clickDrum(letter)}> 
+            <audio className="clip" src={src}  id={letter} controls>
+                Your browser does not support the audio element.
+            </audio>
+            {letter[0]}
+        </button>
+
+    )
+}
+
+export default MarkdownPreviewer; 
