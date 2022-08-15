9179651c8b75e59ea038acad6285dc892f77fba2
Adrian John <adrianjohn.developer@gmail.com>
2022-08-15 12:23:03 -0400

    routing

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,19 +22,19 @@ function App() {
       <BrowserRouter>
         <nav>  
           <ul>
-            <li><Link to="/QuoteMachine">Quotes</Link></li>
-            <li><Link to="/MarkdownPreviewer">MarkdownPreviewer</Link></li>
-            <li><Link to="/DrumMachine">DrumMachine</Link></li>
-            <li><Link to="/Calculator">Calculator</Link></li>
-            <li><Link to="/Clock">Clock</Link></li>
+            <li><Link to="/frontendev-freecodecamp/QuoteMachine">Quotes</Link></li>
+            <li><Link to="/frontendev-freecodecamp/MarkdownPreviewer">MarkdownPreviewer</Link></li>
+            <li><Link to="/frontendev-freecodecamp/DrumMachine">DrumMachine</Link></li>
+            <li><Link to="/frontendev-freecodecamp/Calculator">Calculator</Link></li>
+            <li><Link to="/frontendev-freecodecamp/Clock">Clock</Link></li>
           </ul>  
         </nav>
         <Routes>
-          <Route path="/QuoteMachine" element={<QuoteMachine />}></Route>
-          <Route path="/MarkdownPreviewer" element={<MarkdownPreviewer />}></Route>
-          <Route path="/DrumMachine" element={<DrumMachine />}></Route>
-          <Route path="/Calculator" element={<Calculator />}></Route>
-          <Route path="/Clock" element={<Clock />}></Route>
+          <Route path="/frontendev-freecodecamp/QuoteMachine" element={<QuoteMachine />}></Route>
+          <Route path="/frontendev-freecodecamp/MarkdownPreviewer" element={<MarkdownPreviewer />}></Route>
+          <Route path="/frontendev-freecodecamp/DrumMachine" element={<DrumMachine />}></Route>
+          <Route path="/frontendev-freecodecamp/Calculator" element={<Calculator />}></Route>
+          <Route path="/frontendev-freecodecamp/Clock" element={<Clock />}></Route>
         </Routes>
       </BrowserRouter> 
       
