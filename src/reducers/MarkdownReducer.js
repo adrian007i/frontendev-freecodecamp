fe1eba8090276158c541ff2154cb75ec7869924a
Adrian John <adrianjohn1993@gmail.com>
2022-08-15 11:56:49 -0400

    completed markdown

--- src/reducers/MarkdownReducer.js
+++ src/reducers/MarkdownReducer.js
@@ -1,6 +1,17 @@
 import {KEY_PRESS} from "../constants";
+const initial = `
+# hello Welcome to my React Markdown Previewer!
 
-export const markdownReducer = (state = "", action) => {
+## heading 2
+### Heading 3
+
+**bold** 
+\n
+_italic_  
+
+`;
+
+export const markdownReducer = (state = initial, action) => {
     switch (action.type) {
         case KEY_PRESS:
             return action.value
